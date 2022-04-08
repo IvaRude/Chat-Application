00959062d0a1e677be4eee933d7d6375e253cc69
IvaRude <rudenko.ivan2014@yandex.ru>
2022-04-08 20:51:21 +0400

    Chat is working! Tutorial is done

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,3 +1,4 @@
+# from email import message
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 import json
@@ -14,22 +15,32 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         # Должно быть перед сообщениями!
         await self.accept()
 
-        # await self.channel_layer.group_send(
-        #     self.room_group_name,
-        #     {
-        #         'type': 'tester_message',
-        #         'tester': 'Hello!',
-        #     }
-        # )
-
-    async def tester_message(self, event):
-        tester = event['tester']
-        await self.send(text_data=json.dumps({
-            'tester': tester,
-        }))
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+    
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                # type указывает на нужный метод
+                'type': 'chatroom_message',
+                'message': message,
+                'username': username,
+            }
+        )
+    
+    async def chatroom_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+        }))
